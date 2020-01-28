d2ff8b0102de7bd8766696f52b72cca2a20ba5da
Hamza <hamza.games2000@hotmail.com>
2020-01-29 00:17:17 +0100

    Versie 1.07

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -8,14 +8,15 @@
 
 		$email = ($_POST['email']);
 		$password = ($_POST['password']);
+        $hash = password_hash($password, PASSWORD_DEFAULT);
 
         $query = $conn->query("SELECT * FROM user WHERE email='$email'");
 
 		if ($query->rowCount() > 0) {
 			$data = $query->fetch(PDO::FETCH_ASSOC);
-		    if (password_verify($password, $data['password'])) {
+		    if (password_verify($password, $hash)) {
 				$row = $query->fetch(PDO::FETCH_ASSOC);
-                $_SESSION['email'] = $data['email'];
+                $_SESSION['username'] = $data['username'];
 				$_SESSION['id'] = $data['id'];
 
 				if ($data['is_admin']) {
